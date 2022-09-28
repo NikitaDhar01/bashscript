46659621505459a28ec1564818776b641174dad4
NikitaDhar01 <82872614+NikitaDhar01@users.noreply.github.com>
2022-09-28 11:24:35 +0200

    Create devops_exercise.sh

---
+++ devops_exercise.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y default-jre
+java_version=$(java -version 2>&1 >/dev/null | grep "java version\|openjdk version" | awk ´{print substr($3,2,2)}´)
+
+if ["$java_version" == ""]
+then
+  echo Installing java has failed. No Java version found.
+elif ["$java_version" == "1."]
+then
+  echo An old version of Java installation found.
+elif ["$java_version" -ge 11]
+then
+  echo Java version 11 or greater installed successfully.
+fi
